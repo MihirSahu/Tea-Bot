b16b170a2888c2a76a071f99c453d17345a8a6cd
Tariq A <tachorzyad@gmail.com>
2022-11-03 01:19:19 -0500

    Added basic purge

--- src/Purge.ts
+++ src/Purge.ts
@@ -1,5 +1,14 @@
-import { Client, Routes, ChatInputCommandInteraction } from "discord.js";
-import { token, clientId, guildId } from './Bot';
+import { Client, Routes, ChatInputCommandInteraction, GuildChannelManager, InteractionCollector, Guild, Events, CategoryChannel, TextChannel } from "discord.js";
+import { token, clientId, guildId, client } from './Bot';
 
 const cronJob = require('node-cron');
 
+const channelId = '1037586167111094372';
+
+client.on(Events.ClientReady, async c => {
+    let guild: Guild | null | void  = await c.guilds.cache.get(guildId)
+    const teaChannel = guild?.channels.cache.get(channelId) as TextChannel;
+
+    teaChannel?.clone();
+    teaChannel?.delete();
+});
